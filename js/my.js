1da680dcef0d203a0f2d552f3b745340303605de
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-05-16 17:08:29 +0500

    masala bajarildi

---
+++ js/my.js
@@ -0,0 +1,146 @@
+
+
+// 1-masala
+
+// let L=+prompt("Uzunlikni sm larda kiriting");
+// L=parseInt(L/100);
+// console.log(L);
+
+
+
+// 2-masala
+
+// let M=+prompt("Og'irlikni kg larda kiriting", 15);
+// M=parseInt(M/1000);
+// console.log(M);
+
+
+
+// 3-masala
+
+// let b=+prompt("file hajmi?", 4562)
+// let kb=parseInt(b/1024)
+// console.log(kb);
+
+
+
+
+// 4-masala
+
+// let A=+prompt("1-sonni kiriting"), B=+prompt("2-sonni kiriting");
+// let count=parseInt(A/B)
+// console.log(count);
+
+
+
+
+
+// 5-masala
+
+// let A=+prompt("1-sonni kiriting"), B=+prompt("2-sonni kiriting");
+// let count=parseInt(A/B);
+// let qoldiq=A%B;
+// console.log(count);
+// console.log(qoldiq);
+
+
+
+
+
+// 6-masala
+
+// let a=+prompt("2 xonalik son kiriting");
+// let onlik=parseInt(a/10);
+// let birlik=a%10;
+// console.log(onlik);
+// console.log(birlik);
+
+
+
+
+
+
+// 7-masala
+
+// let b=+prompt("2 xonalik son kiriting");
+// let onlik=parseInt(b/10);
+// let birlik=b%10;
+// let sum=onlik + birlik
+// console.log(sum);
+
+
+
+
+
+
+// 8-masala
+
+// let c=+prompt("2 xonalik son kiriting");
+// let onlik=parseInt(c/10);
+// let birlik=c%10;
+// let newNumber=`${birlik}`+`${onlik}`;
+// console.log(newNumber);
+
+
+
+
+
+
+// 9-masala
+
+// let d=+prompt("3 xonalik son kiriting");
+// let yuzlik=parseInt(d/100);
+// console.log(yuzlik);
+
+
+
+
+
+// 10-masala
+
+// let e=+prompt("3 xonalik son kiriting");
+// let birlik=e%10;
+// let onlik=parseInt((e%100)/10);
+// console.log("onlik: "+onlik);
+// console.log("birlik: "+birlik);
+
+
+
+
+
+
+// 11-masala
+
+// let a=+prompt("3 xonalik son kiriting");
+// let birlik=a%10;
+// let onlik=parseInt((a%100)/10);
+// let yuzlik=parseInt(a/100);
+// let sum=birlik + onlik + yuzlik;
+// console.log(sum);
+
+
+
+// 12-masala
+
+// let c=+prompt("3 xonalik son kiriting");
+// let onlik=parseInt((c%100)/10);
+// let birlik=c%10;
+// let yuzlik=parseInt(c/100);
+// let newNumber=`${birlik}`+`${onlik}` + `${yuzlik}`;
+// console.log(newNumber);
+
+
+
+
+
+
+
+// 13-masala
+
+// let a=+prompt("3 xonalik son kiriting");
+// let yuzlik=parseInt(a/100);
+// let ikkiXonalikSon=a%100;
+// let newSon=`${ikkiXonalikSon}`+`${yuzlik}`
+// console.log(newSon);
+
+
